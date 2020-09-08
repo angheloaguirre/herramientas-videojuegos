cafb988995ad846ddf7994cab2b1c2118edf3cff
Jerson La Torre <jerson.latorre@gmail.com>
2020-09-08 10:12:13 -0500

    first commit

---
+++ 1_introducción/MapLoader/Assets/MapLoader.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.IO;
+
+public class MapLoader : MonoBehaviour
+{
+  StreamReader reader;
+
+  void Start()
+  {
+    // crea el lector del archivo
+    reader = new StreamReader("Data/mapa.txt");
+
+    // pone toda la información del archivo en un string
+    string mapData = reader.ReadToEnd();
+
+    // se hace un split por línea
+    string[] lineData = mapData.Trim().Split('\n');
+
+    // se interpreta cada línea
+    for (int i = 0; i < lineData.Length; i++)
+    {
+      string tileData = lineData[i];
+      // se hace un split por comas
+      string[] tileCoordinates = tileData.Split(',');
+
+      // y se lee convierte cadda valor a entero
+      int x = int.Parse(tileCoordinates[0]);
+      int y = -int.Parse(tileCoordinates[1]);
+
+      // se crea el tile en esas coordenadas
+      GameObject tile = Instantiate(Resources.Load<GameObject>("Box"));
+      tile.transform.position = new Vector2(x - 3.5f, y + 3.5f);
+      tile.transform.parent = GameObject.Find("Map").transform;
+    }
+  }
+}
---
+++ 1_introducción/README.md
@@ -0,0 +1,18 @@
+## 1. Herramientas para videojuegos
+
+> (Versión a usar en el curso ⇢ **Unity 2019LTS**)
+
+
+
+### Tipos de herramientas:
+
+- **Herramientas Internas** (Unity)
+  - Crear interfaces en el inspector para facilitar la creación de nuestro vídeojuego.
+- **Herramientas externas**
+  - Programas externos.
+  - Pueden ser desarrollados en distintos lenguajes y por distintas personas.
+
+### Funcionamiento de una herramienta externa:
+
+- **Herramienta** **Externa**   ⇢   .png, .json, .txt, .xml, .bin  ⇢  **Videojuego / Unity**
+
